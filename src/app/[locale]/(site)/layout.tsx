86113a6313ca844fd14c20c50121076e43e6b5a6
hy2510 <hy2510@gmail.com>
2024-12-26 10:53:39 +0900

    2024-12-26 (10:55)
    학교, 학원 계약 종료시 차단 화면

--- src/app/[locale]/(site)/layout.tsx
+++ src/app/[locale]/(site)/layout.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image'
 import SiteLayoutComponent from './SiteLayoutComponent'
+import Link from 'next/link'
 
 export const metadata = {
   title: 'Reading Gate',
@@ -6,5 +8,26 @@ export const metadata = {
 }
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
-  return <SiteLayoutComponent>{children}</SiteLayoutComponent>
+  return <SiteLayoutComponent><SchoolBlocked />{children}</SiteLayoutComponent>
+}
+
+// 학교 만료시 보여줄 화면
+function SchoolBlocked() {
+  return (
+    <div style={{position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, zIndex: 9999, backgroundColor: '#fff', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <Link href={'https://readinggate.com/'}>
+        <Image src='/src/images/@home/closeschool.png' width={700} height={700} style={{backgroundColor: '#f0f0f0', width: '100%', height: 'auto', maxWidth: '700px'}} alt=''/>
+      </Link>
+    </div>
+  )
+}
+
+// 학원 만료시 보여줄 화면
+function AcaBlocked() {
+  return (
+    <div style={{position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, zIndex: 9999, backgroundColor: '#fff', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '20px'}}>
+      <Image src='/src/images/@home/logo_color_full.svg' width={280} height={40} style={{width: '100%', height: 'auto', maxWidth: '280px'}} alt=''/>
+      <div style={{fontSize: '1.4em', marginBottom: '40px', textAlign: 'center'}}>본 사이트는 계약 종료로 인해 사용이 중지 되었습니다.</div>
+    </div>
+  )
 }
